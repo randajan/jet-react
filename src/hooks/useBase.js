075bee2c46a63cf3ecc48e0f7e0d50f728b1453e
Jan Randa <jnranda@gmail.com>
2023-09-29 18:52:21 +0200

    ADD: extend with base.use(path, autoGet)

--- src/hooks/useBase.js
+++ src/hooks/useBase.js
@@ -1,10 +1,10 @@
 import { BaseSync, BaseAsync } from "@randajan/jet-base";
 import { useEffect, useState } from "react";
 
-function use(path) {
+function use(path, autoGet=false) {
     const [[get, getChanges], setGetChanges] = useState([_=>[]]);
     useEffect(_=>this.watch(path, (get, cngs)=>setGetChanges([get, cngs])), [path]);
-    return [ get, getChanges ];
+    return [ autoGet ? get() : get, getChanges ];
 }
 
 BaseSync.prototype.use = use;

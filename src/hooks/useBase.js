9bead41b06abfa4cf611872d1701fa7b48114b94
Jan Randa <jnranda@gmail.com>
2023-09-29 18:58:15 +0200

    BF: Base.use default state

--- src/hooks/useBase.js
+++ src/hooks/useBase.js
@@ -2,7 +2,7 @@ import { BaseSync, BaseAsync } from "@randajan/jet-base";
 import { useEffect, useState } from "react";
 
 function use(path, autoGet=false) {
-    const [[get, getChanges], setGetChanges] = useState([_=>[]]);
+    const [[get, getChanges], setGetChanges] = useState([_=>{}, _=>[]]);
     useEffect(_=>this.watch(path, (get, cngs)=>setGetChanges([get, cngs])), [path]);
     return [ autoGet ? get() : get, getChanges ];
 }

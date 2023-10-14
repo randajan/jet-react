68d70666890ebd0b0fd5317755e19730e913becb
Jan Randa <jnranda@gmail.com>
2023-10-14 11:23:57 +0200

    BF: missing export

--- src/elements/dom/index.js
+++ src/elements/dom/index.js
@@ -1,10 +1,12 @@
-import { Link } from "./Link";
+import { Link, LinkTel, LinkMail } from "./Link";
 import { Route } from "./Route";
 import { Router, useRoute } from "./Router";
 
 
 export {
     Link,
+    LinkTel,
+    LinkMail,
     Route,
     Router,
     useRoute
